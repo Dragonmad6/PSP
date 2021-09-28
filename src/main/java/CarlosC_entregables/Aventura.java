2fa9f0a8f9b52e14cd731371b69e4e9e51da4f48
Carlos <ccalvocalzado@gmail.com>
2021-09-28 18:52:53 +0200

    Aventura

--- src/main/java/CarlosC_entregables/Aventura.java
+++ src/main/java/CarlosC_entregables/Aventura.java
@@ -14,12 +14,14 @@ import java.util.Scanner;
 public class Aventura {
 
     Personaje prota = new Personaje();
-
-    Personaje ninfa = new Personaje("Mercedes", "ninfa", "baston", 3, 2, 4, 3, 0, 2, 3, 2, 1);
-    Personaje botones = new Personaje("Bob", "minotauro", "baston", 4, 3, 1, 2, 1, 1, 2, 1, 3);
-    Personaje ninfa = new Personaje("Mercedes", "ninfa", "baston", 3, 2, 4, 3, 0, 2, 3, 2, 1);
+    
+    Personaje ninfa = new Personaje("Mercedes", "ninfa", "baston", 3, 2, 4, 3, 0, 2, 3, 2, 1, 3);
+    Personaje botones = new Personaje("Bob", "minotauro", "baston", 4, 3, 1, 2, 1, 1, 2, 1, 3, 2);
+    
+    Personaje[] grupo = new Personaje[4];
     Prueba foso = new Prueba("Foso", 2, 3);
-
+    
+    
     public Aventura() {
     }
 
@@ -27,6 +29,8 @@ public class Aventura {
 
         Introduccion();
         SaltarFoso();
+        PeleaAMuerte(prota,ninfa);
+        
     }
 
     public int LanzarDados(int numeroDados) {
@@ -39,6 +43,15 @@ public class Aventura {
             }
         }
         return exitos;
+    }
+    
+    public void Golpear(Personaje atacante, Personaje defensor){
+        int daño = LanzarDados(atacante.fuerza + atacante.destreza - defensor.astucia);
+        //defensor.vida = defensor.vida - daño;
+        defensor.vida -= daño;
+        System.out.println("\n" + atacante.nombre + "golpea a " + defensor.nombre + "causandole: " + daño + " puntos de daño.\n");
+        System.out.println("\n" + defensor.nombre + "le quedan" + defensor.vida + " puntos de vida");
+ 
     }
 
     public void CrearProta() {
@@ -79,11 +92,7 @@ public class Aventura {
 
         System.out.println("Aplomo: ");
         prota.aplomo = Integer.parseInt(sc.nextLine());
-        prota.vida = 5 + constitucion;
-    }
-    
-    public void CrearGrupo(){
-        
+        prota.vida = 5 + prota.constitucion;
     }
 
     public void Introduccion() {
@@ -92,16 +101,77 @@ public class Aventura {
     }
 
     public void SaltarFoso() {
-        int exitos;
-        for (int i = 0; i < grupo.length , i++) {
-            System.out.println(prota.nombre + "Intenta saltar el foso.");
+        for (int i = 0; i < grupo.length; i++) {
+            System.out.println(grupo[i].nombre + "intenta saltar el foso");
             int exitos = LanzarDados(prota.fuerza + prota.destreza);
             System.out.println("Exitos: " + exitos);
-            if (LanzarDados(prota.fuerza + prota.destreza) >= foso.dificultad) {
-                System.out.println(prota.nombre + "Salta el foso");
-            } else {
-                System.out.println(prota.nombre + "Se cae al foso y sufre " + foso.daño + " puntos de daño");
+            if (LanzarDados(prota.fuerza + prota.destreza) >=  foso.dificultad) {
+                System.out.println("");
+                System.out.println(prota.nombre + "salta el foso.");
+            }else{
+                System.out.println(prota.nombre + "se cae al foso y sufre " + foso.daño + "puntos de daño.");
+                
+            }
+            if(LanzarDados(prota.fuerza + prota.destreza) >= foso.dificultad){
+                System.out.println("");
+                System.out.println(prota.nombre + "se cae al foso y sufre " + foso.daño + "puntos de daño.");
             }
         }
     }
+    
+    public void CrearGrupo(){
+        grupo[0] = prota;
+        grupo[1] = ninfa;
+        grupo[2] = botones;
+    }
+    
+    public void PeleaAMuerte(Personaje personaje1, Personaje personaje2){
+        System.out.println("\n" + personaje1.nombre + "se va a pelear con " + personaje2.nombre);
+        while(personaje1.vida > 0 && personaje2.vida > 0){
+            if(personaje1.vida > 0){
+                Golpear(personaje1, personaje2);
+            }
+            if(personaje2.vida > 0){
+                Golpear(personaje2, personaje1);
+            }
+        }
+        if(personaje1.vida > 0 && personaje2.vida <= 0){
+            System.out.println(personaje1.nombre + " ha ganado la pelea.");
+        }else{
+            System.out.println(personaje2.nombre + " ha ganado la pelea.");
+
+        }
+    }
+    
+    public void PeleaEnGrupo(Personaje[] grupo, Personaje enemigo){
+        System.out.println("\n El grupo de aventureros se dispone a enfrentarse a " + enemigo.nombre);
+        int objetivo = grupo.length;
+        while(grupo[0].vida > 0 && enemigo.vida > 0){
+            for (int i = 0; i < grupo.length; i++) {
+                if(grupo[i].vida > 0){
+                    Golpear(grupo[i], enemigo);
+                }
+            }
+            if(enemigo.vida > 0){
+                Golpear(enemigo, grupo[objetivo]);
+                if(grupo[objetivo].vida <= 0){
+                    objetivo--;
+                }
+            }
+        }
+        if(enemigo.vida <= 0){
+            System.out.println("\nEl grupo ha ganado la pelea, ha derrotado a " + enemigo.nombre);
+        }else{
+            System.out.println("\n" + enemigo.nombre + " ha vencido al grupo. ¿Será este el fin?");
+        }
+    }
+    
 }
+
+
+
+
+
+
+
+
--- src/main/java/CarlosC_entregables/Personaje.java
+++ src/main/java/CarlosC_entregables/Personaje.java
@@ -39,4 +39,110 @@ public class Personaje {
         this.aplomo = aplomo;
         this.vida = 5 + constitucion;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+
+    public int getConstitucion() {
+        return constitucion;
+    }
+
+    public void setConstitucion(int constitucion) {
+        this.constitucion = constitucion;
+    }
+
+    public int getCarisma() {
+        return carisma;
+    }
+
+    public void setCarisma(int carisma) {
+        this.carisma = carisma;
+    }
+
+    public int getManipulacion() {
+        return manipulacion;
+    }
+
+    public void setManipulacion(int manipulacion) {
+        this.manipulacion = manipulacion;
+    }
+
+    public int getCompostura() {
+        return compostura;
+    }
+
+    public void setCompostura(int compostura) {
+        this.compostura = compostura;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public int getAstucia() {
+        return astucia;
+    }
+
+    public void setAstucia(int astucia) {
+        this.astucia = astucia;
+    }
+
+    public int getAplomo() {
+        return aplomo;
+    }
+
+    public void setAplomo(int aplomo) {
+        this.aplomo = aplomo;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    
 }
