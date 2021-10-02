2e3d0a77d6de7d0b95e2795d727194d3c730ac73
Carlos <ccalvocalzado@gmail.com>
2021-10-02 03:11:58 +0200

    casa

--- src/main/java/CarlosC_entregables/Aventura.java
+++ src/main/java/CarlosC_entregables/Aventura.java
@@ -14,27 +14,47 @@ import java.util.Scanner;
 public class Aventura {
 
     Personaje prota = new Personaje();
-    
-    Personaje ninfa = new Personaje("Meliany", "ninfa", "baston", 3, 2, 4, 3, 0, 2, 3, 2, 1, 3);
+
+    //Pertenecientes a grupo
+    Personaje ninfa = new Personaje("Meliany", "ninfa", "baston", 3, 2, 4, 3, 0, 2, 3, 2, 1, 4);
     Personaje minotauro = new Personaje("Bob", "minotauro", "maza", 4, 3, 5, 2, 1, 1, 2, 1, 3, 2);
-    Personaje escarabajo = new Personaje("Larvesta", "insecto", "residuos", 5, 1, 4, 2, 0, 5, 4, 3, 1, 1);
-    
+    Personaje escarabajo = new Personaje("Larvesta", "insecto", "residuos", 5, 1, 4, 2, 0, 5, 4, 3, 1, 2);
+
+    //Pertenecientes a grupomobs
+    Personaje cienpies = new Personaje("Cimpios", "cienpies", "dientes", 1, 2, 2, 2, 0, 2, 2, 2, 2, 2);
+    Personaje gusano = new Personaje("Gusanito", "gusano", "Golpe Cuerpo", 1, 2, 1, 1, 0, 1, 1, 1, 1, 1);
+    Personaje termita = new Personaje("Terminator", "termita", "pinzas", 3, 1, 2, 1, 1, 1, 3, 1, 1, 1);
+    Personaje arana = new Personaje("Terminator", "termita", "pinzas", 3, 1, 2, 1, 1, 1, 3, 1, 1, 1);
+
     Personaje[] grupo = new Personaje[4];
-    Personaje bossfinal = new Personaje ("Ragnork", "BESTIA", "Martillo", 5, 5, 5, 4, 1, 3, 3, 3, 2, 4);
+    Personaje[] grupomobs = new Personaje[4];
+    Personaje gnomo = new Personaje("Javier", "Gnomo", "rastrillo", 1, 2, 2, 4, 2, 3, 1, 2, 2, 5);
+    Personaje bossfinal = new Personaje("Ragnork", "BESTIA", "Martillo", 5, 5, 5, 4, 1, 3, 3, 3, 2, 4);
+
     Prueba grieta = new Prueba("Grieta", 2, 3);
-    
-    
+    Prueba acertijo = new Prueba("Acertijo", 2, 1);
+    Prueba fatiga = new Prueba("Fatiga", 1, 1);
+
     public Aventura() {
     }
 
     public void IniciarAventura() {
 
         Introduccion();
+        ComienzoEvento();
         CrearProta();
         CrearGrupo();
+        CrearGrupoMOBS();
+        ComienzoEvento();
         DecisionCamino();
-        //PrimeraLucha();
-        //PeleaEnGrupo(grupo,bossfinal);
+        ComienzoEvento();
+        PrimeraLucha(grupo, grupomobs);
+        ComienzoEvento();
+        TomarDescanso();
+        ComienzoEvento();
+        ElAcertijo();
+        ComienzoEvento();
+        PeleaEnGrupo(grupo, bossfinal);
     }
 
     public int LanzarDados(int numeroDados) {
@@ -48,14 +68,14 @@ public class Aventura {
         }
         return exitos;
     }
-    
-    public void Golpear(Personaje atacante, Personaje defensor){
+
+    public void Golpear(Personaje atacante, Personaje defensor) {
         int daño = LanzarDados(atacante.fuerza + atacante.destreza - defensor.astucia);
         //defensor.vida = defensor.vida - daño;
         defensor.vida -= daño;
         System.out.println("\n" + atacante.nombre + " golpea a " + defensor.nombre + " causandole: " + daño + " puntos de daño.\n");
         System.out.println("\n" + defensor.nombre + " le quedan " + defensor.vida + " puntos de vida");
- 
+
     }
 
     public void CrearProta() {
@@ -68,7 +88,7 @@ public class Aventura {
         prota.especie = sc.nextLine();
         System.out.println("Arma: ");
         prota.arma = sc.nextLine();
-        
+
         System.out.println("Fuerza: ");
         prota.fuerza = Integer.parseInt(sc.nextLine());
 
@@ -92,7 +112,7 @@ public class Aventura {
 
         System.out.println("Astucia: ");
         prota.astucia = Integer.parseInt(sc.nextLine());
-        
+
         System.out.println("Aplomo: ");
         prota.aplomo = Integer.parseInt(sc.nextLine());
         prota.vida = 5 + prota.constitucion;
@@ -110,121 +130,218 @@ public class Aventura {
     }
 
     public void SaltarGrieta() {
-        System.out.println("Empezamos a caminar en la direccion que habiamos tomado, para nuestra sorpresa vemos una pequeña grieta la cual podiamos intentar saltar.");
+        System.out.println("Empezamos a caminar en la direccion que habiamos tomado, para nuestra sorpresa vemos una pequeña grieta la cual podiamos intentar saltar.\n");
         for (int i = 0; i < grupo.length; i++) {
             System.out.println(grupo[i].nombre + " intenta saltar la grieta");
-            
+
             int exitos = LanzarDados(grupo[i].fuerza + grupo[i].destreza);
             System.out.println("Exitos: " + exitos);
-            
-            if (LanzarDados(grupo[i].fuerza + grupo[i].destreza) >=  grieta.dificultad) {
+
+            if (LanzarDados(grupo[i].fuerza + grupo[i].destreza) >= grieta.dificultad) {
                 System.out.println("");
                 System.out.println(grupo[i].nombre + " salta la grieta.");
-            }else{
+            } else {
                 System.out.println(grupo[i].nombre + " se cae a la grieta y sufre " + grieta.daño + " puntos de daño.");
             }
         }
     }
-    
-    public void CrearGrupo(){
+
+    public void CrearGrupo() {
         grupo[0] = prota;
         grupo[1] = ninfa;
         grupo[2] = minotauro;
         grupo[3] = escarabajo;
     }
-    
-    public void PeleaAMuerte(Personaje personaje1, Personaje personaje2){
+
+    public void CrearGrupoMOBS() {
+        grupomobs[0] = cienpies;
+        grupomobs[1] = gusano;
+        grupomobs[2] = termita;
+        grupomobs[3] = arana;
+    }
+
+    public void PeleaAMuerte(Personaje personaje1, Personaje personaje2) {
         System.out.println("\n" + personaje1.nombre + " se va a pelear con " + personaje2.nombre);
-        
-            if(personaje1.vida > 0){
-                Golpear(personaje1, personaje2);
-            }
-            if(personaje2.vida > 0){
-                Golpear(personaje2, personaje1);
-            }
-        
-        if(personaje1.vida > 0 && personaje2.vida <= 0){
+
+        if (personaje1.vida > 0) {
+            Golpear(personaje1, personaje2);
+        }
+        if (personaje2.vida > 0) {
+            Golpear(personaje2, personaje1);
+        }
+
+        if (personaje1.vida > 0 && personaje2.vida <= 0) {
             System.out.println(personaje1.nombre + " ha ganado la pelea.");
-        }else{
+        } else {
             System.out.println(personaje2.nombre + " ha ganado la pelea.");
 
         }
-        if(escarabajo.vida > minotauro.vida){
+        if (escarabajo.vida > minotauro.vida) {
             System.out.println("Larvesta se levanta tras el combate y le dice al minotauro.\n");
             System.out.println("Larvesta: Minotauro no es necesario pelear, debemos proseguir nuestro camino");
             System.out.println("\n 'Le ayuda a levantarse.'");
-        }else{
-            System.out.println("Levanta insecto no eres rival para mí.");
+        } else {
+            System.out.println("Bob: Levanta insecto no eres rival para mí.");
             System.out.println("\n" + ninfa.nombre + " se acerca a " + escarabajo.nombre + " y lo cura");
             escarabajo.vida = escarabajo.vida + 1;
         }
     }
-    
-    public void DecisionCamino(){
+
+    public void DecisionCamino() {
         Scanner ScannerInt = new Scanner(System.in);
-        
+
         System.out.println("Nuestros aventureros se están acercando a una bifurcación que deberían de hacer...\n");
         System.out.println(minotauro.nombre + " el " + minotauro.especie + " piensa que deberíamos de ir por el camino de la izquierda\n");
         System.out.println(escarabajo.nombre + " el " + escarabajo.especie + " piensa que deberíamos de ir por el camino de la derecha\n");
         System.out.println("¿A quién deberíamos de hacer caso?\n");
-        
+
         System.out.println("Pulsa 1 para ir a la izquierda.");
         System.out.println("Pulsa 2 para ir a la derecha");
-        
-        
+
         int camino;
         camino = ScannerInt.nextInt();
-        switch (camino){
+        switch (camino) {
             case 1:
                 System.out.println("Has decidido ir por el camino de la izquierda.");
                 System.out.println("Nuestro amigo el escarabajo " + escarabajo.nombre + " agacha la cabeza y no rechista sabiendo qeu no puede hacer frente al minotauro.");
                 System.out.println("Nuestra compañera " + ninfa.nombre + " anima al escarabajo y proseguimos nuestra camino.");
                 SaltarGrieta();
                 break;
-            
+
             case 2:
                 System.out.println("Has decidido seguir el instinto del insecto y prosigues tu camino.");
                 System.out.println("Nuestro compañero el minotauro " + minotauro.nombre + " no está muy contento con nuestra decisión.");
                 System.out.println("Nuestra compañera " + ninfa.nombre + " intenta relajarlo, pero este lo impide, proseguimos nuestro camino.");
                 PeleaAMuerte(minotauro, escarabajo);
                 break;
-            
+
         }
     }
-    
-    public void PrimeraLucha(){
-        System.out.println("Consiguen llegar a la primera parte de nuestro tronco.");        
+
+    public void ElAcertijo() {
+        System.out.println("Llegando ya al tronco...");
+        System.out.println("Nos encontramos con " + gnomo.nombre + " el " + gnomo.especie);
+        System.out.println("Gnomo: No os dejaré pasar hasta que adivineis mi acertijo.");
+        System.out.println("Estás escapando de un laberinto, y hay tres puertas frente a ti."
+                + "\n La puerta de la izquierda conduce a un infierno. "
+                + "\nLa puerta en el centro te lleva a un asesino mortal."
+                + "\n La puerta de la derecha te conduce a un león que no ha comido en tres meses."
+                + "\n ¿Qué puerta eliges?\n");
+
+        for (int i = 0; i < grupo.length; i++) {
+            System.out.println(grupo[i].nombre + " intenta el acertijo");
+
+            int exitos = LanzarDados(grupo[i].inteligencia + grupo[i].astucia);
+            System.out.println("Exitos: " + exitos);
+
+            if (exitos >= acertijo.dificultad) {
+                System.out.println("");
+                System.out.println(grupo[i].nombre + " acierta el acertijo y lo dejan pasar.");
+            } else {
+                System.out.println(grupo[i].nombre + " falla la respuesta " + acertijo.daño + " puntos de daño con el " + gnomo.arma);
+            }
+        }
+
     }
-    
-    public void PeleaEnGrupo(Personaje[] grupo, Personaje enemigo){
-        System.out.println("\n El grupo de aventureros se dispone a enfrentarse a " + enemigo.nombre);
-        int objetivo = grupo.length;
-        while(grupo[0].vida > 0 && enemigo.vida > 0){
+
+    public void PrimeraLucha(Personaje[] grupo, Personaje[] grupomobs) {
+        System.out.println("Consiguen llegar a la primera parte de nuestro tronco.");
+        System.out.println("Pero en el camino derrepente aparecen diversos enemigos.");
+        System.out.println("De la nada se avalanzan hasta nosotros tres criaturas eran:\n");
+
+        System.out.println(cienpies.nombre + " el " + cienpies.especie);
+        System.out.println(gusano.nombre + " el " + gusano.especie);
+        System.out.println(termita.nombre + " la " + termita.especie);
+
+        System.out.println("\n El grupo de aventureros se dispone a luchar contra los bichos");
+        int objetivo = grupo.length - 1;
+        int enemigos = grupomobs.length - 1;
+        while (grupo[0].vida > 0 && grupomobs[0].vida > 0) {
             for (int i = 0; i < grupo.length; i++) {
-                if(grupo[i].vida > 0){
-                    Golpear(grupo[i], enemigo);
+                if (grupo[i].vida > 0) {
+                    Golpear(grupo[i], grupomobs[i]);
+                }
+                if (grupomobs[i].vida > 0) {
+                    Golpear(grupomobs[objetivo], grupo[enemigos]);
+                }
+                if (grupo[i].vida > 0 && grupomobs[i].vida <= 0) {
+                    System.out.println(grupo[i].nombre + " ha ganado la pelea.");
+                } else {
+                    System.out.println(grupomobs[i].nombre + " ha ganado la pelea.");
+
                 }
             }
-            if(enemigo.vida > 0){
-                Golpear(enemigo, grupo[objetivo]);
-                if(grupo[objetivo].vida <= 0){
+        }
+    }
+
+    public void PeleaEnGrupo(Personaje[] grupo, Personaje finals) {
+        System.out.println("Después de una larga travesia nuestro grupo exausto llega a la copa del arbol.");
+        System.out.println("Lo que no sabían es que les esperaba una horrible criatura indescriptible para los ojos de nuestros protagonistas,");
+        System.out.println("\nEl grupo de aventureros se dispone a enfrentarse a " + finals.nombre);
+        System.out.println(prota.nombre + " levanta su " + prota.arma + "y se dispone a luchar.");
+
+        int objetivo = grupo.length - 1;  //4
+        while (grupo[0].vida > 0 && finals.vida > 0) {
+            for (int i = 0; i < grupo.length; i++) {
+                if (grupo[i].vida > 0) {
+                    Golpear(grupo[i], finals);
+                }
+            }
+            if (finals.vida > 0) {
+                Golpear(finals, grupo[objetivo]); //grupo[4];
+                if (grupo[objetivo].vida <= 0) {
                     objetivo--;
                 }
             }
         }
-        if(enemigo.vida <= 0){
-            System.out.println("\nEl grupo ha ganado la pelea, ha derrotado a " + enemigo.nombre);
-        }else{
-            System.out.println("\n" + enemigo.nombre + " ha vencido al grupo. ¿Será este el fin?");
+        if (finals.vida <= 0) {
+            System.out.println("\nEl grupo ha ganado la pelea, ha derrotado a " + finals.nombre);
+        } else {
+            System.out.println("\n" + finals.nombre + " ha vencido al grupo. El martillo de la esperanza se desvanece.");
+            System.out.println("Ya que no existia esperanza ni para " + finals.nombre + "ni para nuestros aventureros.");
         }
     }
-    
+
+    public void ComienzoEvento() {
+        System.out.println("\n---------------------------------------------------\n");
+    }
+
+    public void TomarDescanso() {
+        Scanner ScannerInt = new Scanner(System.in);
+        System.out.println("Nuestros heroes depsues de la ultima pelea se ven cansados\n");
+
+        System.out.println("Pulsa 1 para proponer descansar un rato y recuperar fuerzas.");
+        System.out.println("Pulsa 2 para continuar total ya estamos al lado.\n");
+
+        int descanso;
+        descanso = ScannerInt.nextInt();
+        switch (descanso) {
+            case 1:
+                System.out.println("Todos toman un descanso y te lo agradecen.");
+                for (int i = 0; i < grupo.length; i++) {
+                    System.out.println(grupo[i].nombre + " obtiene un punto de vida.");
+                    grupo[i].fuerza = grupo[i].fuerza + 1; 
+                    System.out.println("La fatiga a desaparecido");
+                }
+                break;
+
+            case 2:
+                System.out.println("Has decidido no tomar el descanso.");
+                for (int i = 0; i < grupo.length; i++) {
+                    System.out.println(grupo[i].nombre + " comienza a tener fatiga");
+
+                    int exitos = LanzarDados(grupo[i].fuerza + grupo[i].compostura);
+                    System.out.println("Exitos: " + exitos);
+
+                    if (LanzarDados(grupo[i].fuerza + grupo[i].compostura) >= fatiga.dificultad) {
+                        System.out.println("");
+                        System.out.println(grupo[i].nombre + " no se cansa demasiado se recupera rapidamente.");
+                    } else {
+                        System.out.println(grupo[i].nombre + " se cae al suelo del agotamiento sufre" + fatiga.daño + " puntos de daño.");
+                    }
+                }
+                break;
+
+        }
+    }
 }
-
-
-
-
-
-
-
-
